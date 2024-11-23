52c02024d62725b328e6265c93e78f94b5f3078c
Tom <infodinokk@gmail.com>
2024-11-23 17:05:33 +0700

    1.0.3

--- examples/tutor09_webmonitor/tutor09_webmonitor.ino
+++ examples/tutor09_webmonitor/tutor09_webmonitor.ino
@@ -5,13 +5,18 @@ extern bool USE_PLC_RTC;
 extern int  RTC_SYNC_DURATION;
 extern bool USE_PLC_MODBUS_TCP;
 extern int  MAC_INDEX;
+extern byte mac[];  // byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+extern int MAXD_MQTT;
+extern int MAXM_MQTT;
 
 void setup(){
+  MAXD_MQTT           = 100;
+  MAXM_MQTT           = 100;
   USE_PLC_MODBUS      =  true;                  //- Use PLC Modbus function
   USE_PLC_RTC         =  true;                  //- Use PLC RTC function
   RTC_SYNC_DURATION   =  60;                    //- RTC Sync time (Min.)
   USE_PLC_MODBUS_TCP  =  true;                  //- Enable Modbus TCP Server
-  MAC_INDEX           =  5;                     //- MAC INDEX
+  //MAC_INDEX           =  5;                     //- MAC INDEX
   initPLC();                                    //- Init FX2N PLC function
 }
 
