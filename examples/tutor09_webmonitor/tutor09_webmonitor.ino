5d21abf0c8588bad18da329c659c63bf689f291d
Tom <infodinokk@gmail.com>
2024-12-20 14:18:41 +0700

    2.0.5

--- examples/tutor09_webmonitor/tutor09_webmonitor.ino
+++ examples/tutor09_webmonitor/tutor09_webmonitor.ino
@@ -15,8 +15,7 @@ void setup(){
   MAXD_MQTT           =  100;
   MAXM_MQTT           =  100;
   USE_PLC_MODBUS      =  true;                  //- Use PLC Modbus function
-  USE_PLC_RTC         =  false;                 //- Use PLC RTC function
-  RTC_SYNC_DURATION   =  60;                    //- RTC Sync time (Min.)
+  USE_PLC_RTC         =  true;                  //- Use PLC RTC function
   USE_PLC_MODBUS_TCP  =  true;                  //- Enable Modbus TCP Server
   initPLC();                                    //- Init FX2N PLC function
 }
